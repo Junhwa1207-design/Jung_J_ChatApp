4d553a117f11c1f205a8614176e79b30c819a52a
ganjunjie <ganjunjie@Eddiede-MacBook-Air.local>
2021-02-19 10:03:14 -0500

    adding design

--- public/scripts/components/TheMessageComponent.js
+++ public/scripts/components/TheMessageComponent.js
@@ -1,10 +1,18 @@
-export default{
-    props:['msg'],
+export default {
+    props:['msg', 'socketid'],
 
     template:
     `
-    <article> 
-    <h1>This is a message </h1>  
+    <article class="new-message" :class="{ 'my-message' : matchedID}"> 
+    
+    <h4>{{msg.message.name}} says:</h4>
+    <p>{{msg.message.content}} </p>
     </article> 
-    `
+    `,
+    
+    data: function() {
+        return {
+            matchedID: this.socketid == this.msg.id
+        }
+    }
 }
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,34 @@
+import ChatMessage from "./components/TheMessageComponent.js"
+
 (() => {
         console.log('fired');
 
         // load the socket library and make a connection
         const socket = io();
 
+        //messenger services event handling -> incoming from the manager
+        function setUserId({sID, message}) {
+                //incoming connected event with data
+                //debugger;
+                vm.socketID = sID;
+         }
+
+        function appendMessage(message) {
+                //debugger;
+                vm.messages.push(message);
+        }
+        function appendUsername(username) {
+                //debugger;
+                vm.username.push(username);
+        }
+
         const vm = new Vue({
                 data: {
                         messages: [],
                         nickname: "",
-                        username: ""
+                        username: "",
+                        socketID: "",
+                        message: ""
                 },
 
                 created: function(){
@@ -16,9 +36,28 @@
                 },
 
                 methods: {
+                        dispatchMessage() {
+                                //debugger;
+                                socket.emit('chatmessage', {content: this.message, name: this.username || ""});
 
+                                this.message = "";
+                        },
+                        dispatchUsername() {
+                                //debugger;
+                                socket.emit('username', {content: this.username, name: this.username || ""});
+
+                                this.username = "";
+                        }
+                },
+
+
+
+                components: {
+                        newmessage: ChatMessage
                 }
         }).$mount("#app");
 
-
+        socket.addEventListener("connected", setUserId);
+        socket.addEventListener('message', appendMessage);
+        socket.addEventListener('username', appendUsername);
 })();
